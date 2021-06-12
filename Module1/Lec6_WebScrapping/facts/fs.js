3f0d8d868466b7d358fe7eaaf8a4c71e1f9a32ed
devesh-pande <deveshdp1998@gmail.com>
2021-06-12 21:53:33 +0530

    selector activity done

--- Module1/Lec6_WebScrapping/facts/fs.js
+++ Module1/Lec6_WebScrapping/facts/fs.js
@@ -5,7 +5,27 @@ let htmlKaData = fs.readFileSync("./index.html", "utf8");
 
 // console.log(htmlKaData);
 
-
+// cheerio is based on jquery
 let ch = cheerio.load(htmlKaData);
 
-console.log(ch);
+// let pTagKaData = ch("p").text();
+// <p>Hey this is a p tag !!</p>
+// console.log(pTagKaData);
+
+let pTagInsideUl = ch("ul p").text();
+// console.log(pTagInsideUl);
+
+// direct children
+let directPTag = ch("ul>p").text();
+// console.log(directPTag);
+
+// classses and ids
+
+// i want main p tags
+let classMainPTags = ch(".mainp").text();
+// console.log(classMainPTags);
+
+// ids -> #main
+let idMain = ch("#main").text();
+console.log(idMain);
+

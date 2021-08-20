36c153f8a5d0b261ded4f77ca592b5d91c52596b
devesh-pande <deveshdp1998@gmail.com>
2021-08-20 18:38:11 +0530

    Formula input completed

--- Module2_DoM/Excel Clone/script.js
+++ Module2_DoM/Excel Clone/script.js
@@ -35,6 +35,46 @@ formulaInput.addEventListener("keydown", function(e){
     }
 
     cellObj.upstream = [] ;
+
+    let formulaArr = typedFormula.split(" ");
+    let cellsInFormula = [];
+
+    for (let i = 0 ; i < formulaArr.length ; i++){
+      if (formulaArr[i] != "+" && 
+          formulaArr[i] != "-" && 
+          formulaArr[i] != "*" && 
+          formulaArr[i] != "/" && 
+          isNaN(formulaArr[i])){
+            cellsInFormula.push(formulaArr[i]);
+      }
+    }
+
+    for (let i = 0 ; i < cellsInFormula.length ; i++) {
+      addToDownstream(cellsInFormula[i], selectedCellAdd);
+    };
+
+    cellObj.upstream = cellsInFormula ;  // [A1, B1]
+
+    let valObj = {};
+    for (let i = 0 ; i < cellsInFormula.length ; i++) {
+      let cellValue = dataObj[cellsInFormula[i]].value ;
+      valObj[cellsInFormula[i]] = cellValue ;
+    }
+
+    for (let key in valObj){
+      typedFormula = typedFormula.replace(key, valObj[key]);
+    }
+
+    let newValue = eval(typedFormula);
+    
+    lastCell.innerText = newValue ;
+
+    cellObj.value = newValue ;
+    let downstream  = cellObj.downstream ;
+
+    for (let i = 0 ; i < downstream.length ; i++) {
+      updateCell(downstream[i]);
+    }
   }
 });
 
@@ -161,21 +201,6 @@ for (let i = 1; i <= 100; i++) {
 
 }
 
-
-
-dataObj["A1"].value = 20;
-dataObj["A1"].downstream = ["B1"];
-dataObj["B1"].formula = "2 * A1";
-dataObj["B1"].upstream = ["A1"];
-dataObj["B1"].value = 40;
-
-let a1cell = document.querySelector("[data-address='A1']")
-let b1cell = document.querySelector("[data-address='B1']")
-
-a1cell.innerText = 20
-b1cell.innerText = 40
-
-
 // C1 = Formula(2*A1)
 // A1 = parent
 // C1 = child
@@ -206,7 +231,6 @@ function removeFromDownstream(parentCell, childCell){
   dataObj[parentCell].downstream = filteredDownstream ;
 
 }
-
 
 
 function updateCell(cell){
@@ -253,5 +277,10 @@ function updateCell(cell){
   for (let i = 0 ; i < downstream.length ; i++){
     updateCell(downstream[i]);
   }
+
+}
+
+
+function addToDownstream(parentCell, childCell){
 
 }
